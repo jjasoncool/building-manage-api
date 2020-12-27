c476be5e299bc33c3595b2932e5cf40429b33784
Aoi <19519928+AoiYamada@users.noreply.github.com>
2020-12-27 22:39:49 +0800

    feat: create user validation

--- src/validators/User.js
+++ src/validators/User.js
@@ -1,12 +1,27 @@
 'use strict';
 
-// const Joi = require('joi');
+const Joi = require('joi');
+const R = require('ramda');
 
 class UserValidator {
   static async create(ctx, next) {
-    // const { body } = ctx.request;
+    try {
+      const { body } = ctx.request;
 
-    await next();
+      const schema = Joi.object({
+        username: Joi.string().alphanum().min(3).max(30).required(),
+        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+      });
+
+      await schema.validateAsync(body, { abortEarly: false });
+
+      await next();
+    } catch (error) {
+      ctx.status = 400;
+      ctx.body = {
+        messages: R.map(({ message }) => message, error.details),
+      };
+    }
   }
 }
 
